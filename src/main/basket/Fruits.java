afb5dd50cce21dd280d4b140a8620df558a012d8
DeLisik <yuliiaharmash@gmail.com>
2021-01-31 23:06:33 -0500

    Added grouping by

--- src/main/basket/Fruits.java
+++ src/main/basket/Fruits.java
@@ -1,9 +1,14 @@
 package main.basket;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
 
 public class Fruits {
     String type;
@@ -11,14 +16,21 @@ public class Fruits {
     String char1;
     String char2;
 
+    public Fruits() {
+    }
+
+
+    public Fruits(String type, String char1, String char2) {
+        this.type = type;
+        this.char1 = char1;
+        this.char2 = char2;
+    }
+
     public Fruits(String type, int age, String char1, String char2){
         this.type=type;
         this.age=age;
         this.char1=char1;
         this.char2=char2;
-    }
-
-    public Fruits() {
     }
 
     public String getType() {
@@ -54,12 +66,25 @@ public class Fruits {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Fruits)) return false;
+        Fruits fruits = (Fruits) o;
+        return Objects.equals(getType(), fruits.getType()) && Objects.equals(getChar1(), fruits.getChar1()) && Objects.equals(getChar2(), fruits.getChar2());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getType(), getChar1(), getChar2());
+    }
+
+    @Override
     public String toString() {
-        return
+        return "Fruits{" +
                 "type='" + type + '\'' +
-                ", age='" + age + '\'' +
                 ", char1='" + char1 + '\'' +
-                ", char2='" + char2;
+                ", char2='" + char2 + '\'' +
+                '}';
     }
 
     public long totalTypes(List<Fruits> list){
@@ -76,5 +101,99 @@ public class Fruits {
         listFruits.forEach(objectFruits -> System.out.println(objectFruits.getType() + " " + objectFruits.getAge()));
 
     }
+
+    public void getGroups (List<Fruits> listObjects) {
+        Map<String, Long> getGroups = listObjects.stream()
+                .collect(groupingBy(Fruits::getType, Collectors.counting()));
+
+        getGroups.forEach((key, value) -> System.out.println(key + ": " + value));
+    }
+
+    public void getGroupTypesAndChars (List<Fruits> listObjects) {
+
+        listObjects.stream().forEach(ob -> {
+            //ob.
+        });
+
+//        Map<Fruits, IntSummaryStatistics> collect = listObjects.stream()
+//                .collect(groupingBy(
+//                        fruit -> new Fruits(fruit.type, fruit.char1, fruit.char2),
+//                        Collectors.summarizingInt(fruit -> fruit.age)));
+//                collect.forEach((k,v) ->{
+//                    k.age= (int) v.getSum();
+//                    System.out.format(k.age+" "+k.type+": "+k.char1+", "+k.char2+"\n");
+//                });
+
+
+
+
+//
+//        Comparator<Fruits> compareFruits =Comparator
+//                .comparing(Fruits::getType)
+//                .thenComparing(Fruits::getChar1)
+//                .thenComparing(Fruits::getChar2);
+//
+//        List<Fruits> sortedFruits =listObjects.stream()
+//                .sorted(compareFruits)
+//                .collect(Collectors.toList(),
+//                        Collectors.counting());
+//        System.out.println(sortedFruits);
+
+
+
+
+
+        //        List<Fruits> newList = new ArrayList<>();
+//
+//        listObjects.forEach(a -> newList.add(new Fruits(a.getType(),
+//                a.getChar1(),
+//                a.getChar2())));
+//
+//
+//
+//
+//        Map<Fruits, Integer> map = newList.stream().
+//                collect(Collectors.toMap(Function.identity(), value -> 1, Integer::sum));
+//        map.forEach((key, value) -> System.out.println(value + ": " + key.getAge() + ": " + key.getChar1() + ", " + key.getChar2()));
+//
+
+        // 1. Set of 3 elements (4 of them)
+        // 2. Run list if list has set -> store as map number, set (loop)
+//        Set<Fruits> fruitsSet = new HashSet<>();
+//
+//        listObjects.forEach(fruitObject -> {
+//            fruitsSet.add(new Fruits(fruitObject.getType(), fruitObject.getChar1(), fruitObject.getChar2()));
+//        });
+//
+//        fruitsSet.forEach(a -> System.out.println(a.getType() + ", " + a.getChar1() +", " + a.getChar2()));
+//        Map<Integer, Set<Fruits>> mapFruits = new HashMap<>();
+//
+//
+//        for (Fruits element : fruitsSet) {
+//            int sum = 0;
+//
+//            for (Fruits fruitObject : listObjects) {
+//                if (element.getType().equals(fruitObject.getType()) && element.getChar1().equals(fruitObject.getChar1())
+//                && element.getChar2().equals(fruitObject.getChar2())) {
+//                    sum++;
+//                }
+//            }
+//            mapFruits.put(sum, fruitsSet);
+//
+//        }
+//        mapFruits.forEach((key, value) -> System.out.println(key + ": " + value));
+
+
+
+
+        // if set has values from the list +1
+
+
+
+//        setOfList.forEach(System.out::println);
+
+
+    }
+
 
 }
